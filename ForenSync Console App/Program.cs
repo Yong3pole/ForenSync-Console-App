49f247ce5af34be3863d5140dfde86dffd59dc3d
Yong3pole <tripo@YONG-LAPPY>
2025-10-06 14:22:29 +0800

    UTF8

--- ForenSync Console App/Program.cs
+++ ForenSync Console App/Program.cs
@@ -1,9 +1,12 @@
 ﻿using ForenSync_Console_App.UI;
+using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
         LoginPage.PromptCredentials();
     }
 }
